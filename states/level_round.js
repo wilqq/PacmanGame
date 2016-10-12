c0eb0ad88acaf5c2235deefbeafca1f5ded6dc0d
Mateusz Wilczyński <matwilq@gmail.com>
2016-10-12 13:33:58 +0200

    Chapter 2: add map

--- states/level_round.js
+++ states/level_round.js
@@ -3,9 +3,13 @@ function LevelRoundState() {};
 LevelRoundState.prototype = {
   preload: function () {
     this.load.image('logo', 'assets/logo.jpg');
+    this.load.image('tiles', 'maps/box-tiles.png');
+    this.load.tilemap('level_1_map', 'maps/level_1_map.json', null, Phaser.Tilemap.TILED_JSON);
   },
   create: function () {
-    this.game.add.sprite(0, 80, 'logo');
+    this.map = this.add.tilemap('level_1_map');
+    this.map.addTilesetImage('tiles', 'tiles');
+    this.layer = this.map.createLayer('Layer1');
   },
   update: function () {
     // body...
